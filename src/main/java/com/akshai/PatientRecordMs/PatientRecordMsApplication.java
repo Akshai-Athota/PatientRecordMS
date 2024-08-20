222b3fc398b4d00f68b2418cfeb48543ed6ae9de
Akshai-Athota <akshaiathotaspring001@gmail.com>
2024-08-20 21:43:04 +0530

    initial set up

---
+++ src/main/java/com/akshai/PatientRecordMs/PatientRecordMsApplication.java
@@ -0,0 +1,13 @@
+package com.akshai.PatientRecordMs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientRecordMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientRecordMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/akshai/PatientRecordMs/PatientRecordMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akshai.PatientRecordMs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientRecordMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
